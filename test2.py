ca3127bf48b95767d6d25a414d305b5361c8956c
Laptop <micahcorbintanner@gmail.com>
2025-02-26 11:35:07 -0700

    Remove Unused Files

--- test2.py
+++
@@ -1,9 +0,0 @@
-from globalVaribles import *
-
-class sapi:
-    def idk(self):
-        # other_file.py
-
-        window=shared_dict['my_global_var']
-        
-        window.evaluate_js('document.write("Please end my suffering");')
